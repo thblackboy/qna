81bd4cbf0834bcddd16cefa8010c04466db8114c
Alex Alfonso <alfonsoalexandr@gmail.com>
2022-02-12 01:39:18 +0300

    add create action to Questions_controller

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+  expose :questions, -> { Question.all }
+  expose :question
+  def create
+    if question.save
+      redirect_to question
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
+end
---
+++ spec/controllers/questions_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe QuestionsController, type: :controller do
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves new question to db' do
+        expect { post :create, params: { question: attributes_for(:question) } }.to change(Question, :count).by(1)
+      end
+
+      it 'redirects to show view' do
+        post :create, params: { question: attributes_for(:question) }
+        expect(response).to redirect_to assigns(:exposed_question)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save new question to db' do
+        expect do
+          post :create, params: { question: attributes_for(:question, :invalid) }
+        end.to_not change(Question, :count)
+      end
+
+      it 're-renders new view' do
+        post :create, params: { question: attributes_for(:question, :invalid) }
+        expect(response).to render_template :new
+      end
+    end
+  end
+end
